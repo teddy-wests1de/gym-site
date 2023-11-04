fbd49f7750c80b3c172037b534d625e56f8f1a9c
Elrico Landrew <dirkieys21@gmail.com>
2023-11-04 22:55:57 +0200

    Adding Classes

--- gym-app.js
+++ gym-app.js
@@ -6,17 +6,16 @@ const loginMenu = document.querySelector('.login-menu');
 const btnLogin = document.querySelector('.btn-login');
 const form = document.querySelectorAll('.form');
 const login = document.querySelectorAll('.login');
-// const screen = document.querySelectorAll('.screen')
 const adminMenu = document.querySelector('.admin-menu')
 const accountLogin = document.querySelector('.account-login');
 const accountRegister = document.querySelector('.account-register')
 const btnProfile = document.querySelector('.btn-profile');
-
+const welcomeText = document.querySelector('.welcome-text');
 class User {
-    constructor(firstName, lastName, id) {
+    constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.id = id;
+        // this.id = id;
     }
 
     _getFullName() {
@@ -29,9 +28,33 @@ class User {
 
 class Member extends User {
     constructor(firstName, lastName, id, memberID) {
-        
+        super(firstName, lastName, id);
+        this.memberID = memberID
+    }
+
+    _getBlogs(blogs) {
+
     }
 }
+
+// class Blog {
+//     id = (Date.now() + '').slice(-10);
+//     blogs = [];
+//     constructor(name, description){
+//         this.name = name;
+//         this.description = description;
+//         console.log(this.id, this.name);
+//     }
+// }
+
+class Trainer extends User {
+    constructor(firstName, lastName, memberID, trainerID) {
+        super(firstName, lastName);
+        this.memberID = memberID;
+        this.trainerID = trainerID;
+    }
+}
+
 
 // Main App
 class App {
@@ -47,6 +70,11 @@ class App {
         
         // Toggle Admin/Profle menu
         btnProfile.addEventListener('click', this._adminOptions.bind(this))
+
+        adminMenu.addEventListener('click', this._adminOptions.bind(this));
+
+        // const blog = new Blog('Test', 'Testing Blog');
+        this._registerUser();
     }
 
     _toggleMenu(e) {
@@ -63,36 +91,58 @@ class App {
         e.preventDefault();
         const clicked = e.target;
         // Guard clause for login menu
-        if(!clicked.classList.contains('profile')) return;
+        if(clicked.classList.contains('profile')) {
+            this._adminOptions(e);
+        }
 
         if(clicked.dataset.action === 'login') {
             this.#loggedIn = true;
             this._getAdminMenu();
             adminMenu.classList.add('hidden');
+            this._checkLogin();
         }
         if(clicked.dataset.action === 'logout') {
             this.#loggedIn = false;
             this._logout();
             adminMenu.classList.add('hidden');
+            this._checkLogin();
         }
 
-        console.log(clicked.dataset.action)
+        // Hide main menu on item clicked
+        if(!clicked.classList.contains('btn-profile')) {
+            console.log(clicked)
+            menu.classList.add('hidden');
+        }
 
     }
 
-    _adminOptions() {
-        adminMenu.classList.toggle('hidden');
+    _adminOptions(e) {
+        if(e.target.dataset.type === 'login') {
+            adminMenu.classList.toggle('hidden');
+        }
+        if(e.target.dataset.type !== 'login') {
+            adminMenu.classList.add('hidden');
+            console.log(adminMenu)
+        }
     }
+
     _registerUser(user) {
-
+        const trainer1 = new Trainer('Riccardo', 'Rosseau', 123, 111);
+        this.#users.push(trainer1);
+        this._listUsers();
     }
 
+    _listUsers () {
+        console.log(this.#users);
+    }
     _checkLogin(e) {
         // e.preventDefault();
         login.forEach(l =>  l.classList.add('hidden'));
-        // this.#loggedIn = true;
-        // console.log(this.#loggedIn);
-        // this._getAdminMenu();
+        if(this.#loggedIn === true) {
+            welcomeText.textContent = `Welcome ${this.#users[0].firstName}`
+        } else {
+            welcomeText.textContent = 'Successfully Logged out...!';
+        }
     }
     
     _getAdminMenu() {
@@ -100,6 +150,7 @@ class App {
         const html = `
         <div class="logout-menu">
             <div class="account profile" data-action="profile">Profile</div>
+            <div class="account profile" data-action="profile">Workouts</div>
             <div class="account profile" data-action="users">Users</div>
             <div class="account profile" data-action="blogs">Blogs</div>
             <div class="account profile logout" data-action="logout">Logout</div>
@@ -119,6 +170,10 @@ class App {
         adminMenu.insertAdjacentHTML('beforeend', html);
     }
 
+
+    _showProfile(user) {
+        
+    }
 }
 
 const app = new App();
